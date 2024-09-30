c43f0217807d10662a7c016a6cc1f6c06eee5d55
Dieghoatc <dieghoatc@gmail.com>
2024-09-30 03:35:46 -0500

    feat: add services section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      secondary: ["Geist Sans", "sans-serif;"],
+    },
     extend: {
       colors: {
+        secondary: "#d1b000",
+      },
+      backgroundColor: {
         secondary: "#d1b000",
       },
     },
